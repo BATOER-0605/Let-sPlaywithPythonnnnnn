b6ffab9b199cd236a8d6f8acf88c46a3b3e4565b
BATOER-0605 <dougahsk@outlook.com>
2021-11-19 14:21:29 +0900

    Done

--- Janken.py
+++ Janken.py
@@ -1,5 +1,10 @@
 import random
 
+#対戦結果の初期化
+win=0
+draw=0
+lose=0
+
 while True:
     print("\nHey!お前ら準備はいいか？？\n")
     print("これからお前にはじゃんけんで遊んでもらう！\n")
@@ -35,97 +40,85 @@ while True:
     elif bots==3:
         botshand='パー'
 
-    #対戦結果の初期化
-    win=0
-    draw=0
-    lose=0
-
     #Botの手札とプレイヤーが出した手札を表示
     print("Botが出した手札:\n",botshand)
     print("あなたがだした手札:",playershand)
 
     #判断フロー
-    while True:
-        if botshand=='グー':
-            if playershand=='グー':
-                print("あいこだ！そーれもういっちょ！！！")
-                draw+=1
+    if botshand=='グー':
+        if playershand=='グー':
+            print("あいこだ！そーれもういっちょ！！！")
+            draw+=1
 
-            if playershand=='チョキ':
-                botres=random.randint(1,3)
-                if botres==1:
-                    print("はっはっはっはっはー！私のほうが強いのだ！")
-                elif botres==2:
-                    print("私の勝ちだ。どうして負けたか考えるがよい。")
-                else:
-                    print("どうやら神は私に味方するようだ。")
+        if playershand=='チョキ':
+            botres=random.randint(1,3)
+            if botres==1:
+                print("はっはっはっはっはー！私のほうが強いのだ！")
+            elif botres==2:
+                print("私の勝ちだ。どうして負けたか考えるがよい。")
+            else:
+                print("どうやら神は私に味方するようだ。")
                 lose+=1
 
-            if playershand=='パー':
-                botres=random.randint(1,3)
-                if botres==1:
-                    print("くっ、、、おぬし、強いなっ、、、！ﾊﾞﾀﾘ")
-                elif botres==2:
-                    print("私の負けだ。君は強いな。")
-                else:
-                    print("どうしてだよｵｵｵｵｵｵｵｵ!!!!!!!!!!!")
+        if playershand=='パー':
+            botres=random.randint(1,3)
+            if botres==1:
+                print("くっ、、、おぬし、強いなっ、、、！ﾊﾞﾀﾘ")
+            elif botres==2:
+                print("私の負けだ。君は強いな。")
+            else:
+                print("どうしてだよｵｵｵｵｵｵｵｵ!!!!!!!!!!!")
                 win+=1
 
-        if botshand=='チョキ':
-            if playershand=='チョキ':
-                print("あいこだ！そーれもういっちょ！！！")
-                draw+=1
+    if botshand=='チョキ':
+        if playershand=='チョキ':
+            print("あいこだ！そーれもういっちょ！！！")
+            draw+=1
 
-            if playershand=='パー':
-                botres=random.randint(1,3)
-                if botres==1:
-                    print("はっはっはっはっはー！私のほうが強いのだ！")
-                elif botres==2:
-                    print("私の勝ちだ。どうして負けたか考えるがよい。")
-                else:
-                    print("どうやら神は私に味方するようだ。")
+        if playershand=='パー':
+            botres=random.randint(1,3)
+            if botres==1:
+                print("はっはっはっはっはー！私のほうが強いのだ！")
+            elif botres==2:
+                print("私の勝ちだ。どうして負けたか考えるがよい。")
+            else:
+                print("どうやら神は私に味方するようだ。")
                 lose+=1
 
-            if playershand=='グー':
-                botres=random.randint(1,3)
-                if botres==1:
-                    print("くっ、、、おぬし、強いなっ、、、！ﾊﾞﾀﾘ")
-                elif botres==2:
-                    print("私の負けだ。君は強いな。")
-                else:
-                    print("どうしてだよｵｵｵｵｵｵｵｵ!!!!!!!!!!!")
+        if playershand=='グー':
+            botres=random.randint(1,3)
+            if botres==1:
+                 print("くっ、、、おぬし、強いなっ、、、！ﾊﾞﾀﾘ")
+            elif botres==2:
+                print("私の負けだ。君は強いな。")
+            else:
+                print("どうしてだよｵｵｵｵｵｵｵｵ!!!!!!!!!!!")
                 win+=1
 
-        if botshand=='パー':
-            if playershand=='パー':
-                print("あいこだ！そーれもういっちょ！！！")
-                draw+=1
+    if botshand=='パー':
+        if playershand=='パー':
+            print("あいこだ！そーれもういっちょ！！！")
+            draw+=1
 
-            if playershand=='グー':
-                botres=random.randint(1,3)
-                if botres==1:
-                    print("はっはっはっはっはー！私のほうが強いのだ！")
-                elif botres==2:
-                    print("私の勝ちだ。どうして負けたか考えるがよい。")
-                else:
-                    print("どうやら神は私に味方するようだ。")
+        if playershand=='グー':
+            botres=random.randint(1,3)
+            if botres==1:
+                print("はっはっはっはっはー！私のほうが強いのだ！")
+            elif botres==2:
+                print("私の勝ちだ。どうして負けたか考えるがよい。")
+            else:
+                print("どうやら神は私に味方するようだ。")
                 lose+=1
 
-            if playershand=='チョキ':
-                botres=random.randint(1,3)
-                if botres==1:
-                    print("くっ、、、おぬし、強いなっ、、、！ﾊﾞﾀﾘ")
-                elif botres==2:
-                    print("私の負けだ。君は強いな。")
-                else:
-                    print("どうしてだよｵｵｵｵｵｵｵｵ!!!!!!!!!!!")
-                    win+=1
-
-    #対戦結果を表示
-    print("対戦結果\n")
-    print("勝った回数\n",win)
-    print("負けた回数\n",lose)
-    print("引き分けの回数\n",draw)
+        if playershand=='チョキ':
+            botres=random.randint(1,3)
+            if botres==1:
+                print("くっ、、、おぬし、強いなっ、、、！ﾊﾞﾀﾘ")
+            elif botres==2:
+                print("私の負けだ。君は強いな。")
+            else:
+                print("どうしてだよｵｵｵｵｵｵｵｵ!!!!!!!!!!!")
+                win+=1
 
     #コンティニューの是非
     print("\n続けますか？(y/n)")
@@ -134,8 +127,18 @@ while True:
         continue
 
     elif playerres=='n' :
+            #対戦結果を表示
+        print("対戦結果\n")
+        print("勝った回数\n",win)
+        print("負けた回数\n",lose)
+        print("引き分けの回数\n",draw)
         break
 
     else :
         print("不明なyかnで答えろ言うてるやん。従わないならここから去るが良い！")
+            #対戦結果を表示
+        print("対戦結果\n")
+        print("勝った回数\n",win)
+        print("負けた回数\n",lose)
+        print("引き分けの回数\n",draw)
         break
